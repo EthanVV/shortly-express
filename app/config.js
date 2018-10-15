a7517ac63c68fc0b9b447b2ac91289b6135bf194
Hack Reactor Student <student@hackreactor.com>
2018-10-15 15:35:10 -0500

    add users schema

--- app/config.js
+++ app/config.js
@@ -40,5 +40,16 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 // Add additional schema definitions below
 /************************************************************/
 
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function(user) {
+      user.increments('id').primary();
+      users.string('username', 25);
+      users.string('password_digest', 255);
+    }).then(function(table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
 module.exports = db;
